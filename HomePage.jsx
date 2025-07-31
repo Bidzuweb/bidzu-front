60f2c8e0f431ac049c13a8668477a1126d0dba53
Bidzuweb <handifyshop@gmail.com>
2025-07-31 17:03:26 -0400

    Update HomePage.jsx

--- HomePage.jsx
+++ HomePage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
   const [listings, setListings] = useState([]);
